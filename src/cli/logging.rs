502b4adfa0fc1442a80c7f26b86ea31a71905a01
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-08-23 22:27:41 +0200

    Improves panic feedback with support link

--- src/cli/logging.rs
+++ src/cli/logging.rs
@@ -1,11 +1,14 @@
 // Copyright 2024 Dotanuki Labs
 // SPDX-License-Identifier: MIT
 
-use human_panic::setup_panic;
+use human_panic::{metadata, setup_panic};
 use log::LevelFilter;
 
 pub fn setup_logging(verbose: bool) {
-    setup_panic!();
+    let homepage = "https://github.io/dotanuki-labs/gradle-wiper";
+    let support_message = format!("For issues, reach out to {}/issues", homepage);
+    setup_panic!(metadata!().support(support_message.clone()).homepage(homepage));
+
     better_panic::install();
 
     let filter = if verbose { LevelFilter::Debug } else { LevelFilter::Info };
