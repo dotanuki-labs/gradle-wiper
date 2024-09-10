ef9761a5198b8c3b6497e09fb6ef5c885882b8f4
Ubiratan Soares <ubiratan.f.soares@gmail.com>
2024-09-10 07:24:08 +0200

    Updates callinectes coordinates

--- krabby.sh
+++ krabby.sh
@@ -7,7 +7,7 @@ set -e
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$dir"
 
-readonly callinectes="ghcr.io/dotanuki-labs/callinectes:85d8d6eed9666fb82ba90c2038dfafd6c3bf70ed"
+readonly callinectes="ghcr.io/dotanuki-labs/callinectes:latest@sha256:3920029aeb8dfabcd26620f631a215eb289950f6b34857fb1deb2f935ef96715"
 readonly output_dir="artifacts"
 readonly e2e_bin_home="target/e2e"
 readonly task="$1"
